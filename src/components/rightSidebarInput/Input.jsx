03e41099023e77ec4a24229b6c6ac643a311fa7f
ahros-code <probroalpha@gmail.com>
2023-05-02 22:03:00 +0500

    fix after fix

--- src/components/rightSidebarInput/Input.jsx
+++ src/components/rightSidebarInput/Input.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { SearchContext } from '../../context/searchContext';
+import { SearchContext } from '../../context/SearchContext';
 import css from './Input.module.css';
 
 const Input = () => {
